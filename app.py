1c67afd096a203af98025c6ee28958bc86246ed7
Heather Worstell <heatherworstell@Heathers-Air.lan>
2026-06-10 17:57:26 -0500

    Fix flake8 formatting

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return jsonify({"message": "Welcome to the Flask App!"})
 
+
 @app.route("/health")
 def health():
     return jsonify({"status": "OK"}), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
